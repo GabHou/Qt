2f560d3ebe77526592cc297411337f118d38b462
GabHou <houzugui@163.com>
2018-09-06 17:37:37 +0800

    WORK_PC

---
+++ selectitem.cpp
@@ -0,0 +1,6 @@
+#include "selectitem.h"
+
+selectItem::selectItem()
+{
+
+}
---
+++ selectitem.h
@@ -0,0 +1,11 @@
+#ifndef SELECTITEM_H
+#define SELECTITEM_H
+
+
+class selectItem : public QGraphicsItem
+{
+public:
+    selectItem();
+};
+
+#endif // SELECTITEM_H
